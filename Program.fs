28899b56f86cfa82d45dc4144883db2901b0e7f1
KPX <dm.mikufans@gmail.com>
2023-07-18 21:24:49 +0800

    init

---
+++ Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open System.IO
+open System.Web
+
+open DownKyi.Core.BiliApi.Login
+open DownKyi.Core.Utils
+
+
+[<EntryPoint>]
+let main argv =
+    Environment.CurrentDirectory <- @"Z:\bili\DownKyi-1.5.9"
+
+    let cookies =
+        if false then
+            Console.Write("Cookie:")
+            Console.ReadLine()
+        else
+            File.ReadAllText("../cookies.txt")
+
+    let url =
+        let uri = UriBuilder("https://fuck-qrcode.org/")
+        let query = HttpUtility.ParseQueryString(uri.Query)
+
+        query.Add("Expires", TimeSpan.FromDays(1.0).TotalSeconds.ToString())
+
+        for kv in cookies.Split(Array.singleton "; ", StringSplitOptions.RemoveEmptyEntries) do
+            let tmp = kv.Split(Array.singleton '=', 2)
+            let value = tmp.[1] |> HttpUtility.UrlDecode
+            query.Add(tmp.[0], value)
+            printfn $"{tmp.[0]}: {value}"
+
+        uri.Query <- query.ToString()
+
+        uri.ToString()
+
+    for cookie in ObjectHelper.ParseCookie(url).GetCookies(Uri("https://www.bilibili.com")) do
+        printfn $"{cookie.Name}:{cookie.Value} -> {cookie.Expires}"
+
+    printfn $"save: {LoginHelper.SaveLoginInfoCookies(url)}"
+    printfn $"dump: {LoginHelper.GetLoginInfoCookiesString()}"
+
+    0
---
+++ README.md
@@ -0,0 +1,7 @@
+# 说明
+
+1. 网页登录
+2. 访问 https://api.bilibili.com/x/web-interface/nav
+3. 使用F12复制cookie值，保存到cookies.txt
+4. 执行代码（不要更改框架，不能使用dotnet fsi运行，用.NET自带的fsi.exe可能能用）
+5. 启动downkyi测试
